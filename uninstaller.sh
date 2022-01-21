36ef7b9883a400045e5423393e8745d1d991a29b
jinwoole <indibooks@naver.com>
2022-01-21 19:38:38 +0900

    add uninstaller

---
+++ uninstaller.sh
@@ -0,0 +1,8 @@
+read -p "neovim과 init.vim을 포함한 .config 폴더가 삭제됩니다. 진행하시겠습니까? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  brew uninstall neovim
+  rm -rf ~/.config
+  echo "삭제 완료"
+else
+  echo "취소"
+fi
