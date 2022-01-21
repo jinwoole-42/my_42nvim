3f8a0aa1cb0516aa9b96754d568cfbd697c473c5
jinwoole <indibooks@naver.com>
2022-01-21 19:30:07 +0900

    update

--- installer.sh
+++ installer.sh
@@ -1,18 +1,14 @@
 brew install neovim
-brew install cask
-brew tap homebrew/cask-fonts
-brew cask install font-hack-nerd-font
 brew install node.js
 brew install python3
-
-alias vim='nvim'
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 mkdir ~/.config/nvim
-
 rm ~/.config/nvim/init.vim
-mv init.vim ~/.config/nvim
+mv ./init.vim ~/.config/nvim
 
-nvim
+echo alias vim='nvim' bash_profile에 등록하면 좋음. 아무튼 좋음.
+echo 단축키 및 사용법 github README.md 참고
+echo 커스텀 시 .config/nvim/init.vim 수정
