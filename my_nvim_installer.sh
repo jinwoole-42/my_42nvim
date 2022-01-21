e8bb20ccdc013880f083bfed4159717803562f8b
jinwoole <indibooks@naver.com>
2022-01-21 20:19:22 +0900

    compltet v1

---
+++ my_nvim_installer.sh
@@ -0,0 +1,17 @@
+brew install neovim
+brew install node.js
+brew install python3
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+mkdir ~/.config
+mkdir ~/.config/nvim
+rm ~/.config/nvim/init.vim
+mv ./init.vim ~/.config/nvim/init.vim
+
+echo --------------------------------------------------------
+echo alias vim='nvim'을 bash_profile에 등록하면 좋음
+echo 단축키 및 사용법 github README.md 참고
+echo 커스텀 하고싶으면 .config/nvim/init.vim 수정
+echo --------------------------------------------------------
