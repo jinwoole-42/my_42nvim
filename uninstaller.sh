8a7f5676142a5c384ee570ca81b9218241cbcda1
jinwoole <jinwoole@student.42seoul.kr>
2022-01-31 14:18:53 +0900

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,9 +1,9 @@
 read -p "neovim과 init.vim, yarn폴더가 삭제됩니다. 진행하시겠습니까? (y/n) " RESP
 if [ "$RESP" = "y" ]; then
   brew uninstall neovim
-  brew uninstall universal-ctags
-  rm ~/.config/nvim/init.vim
-  rm -rm ~/.config/coc
+ 
+  rm -rf ~/.config/nvim
+  rm -rf ~/.config/coc
   rm -rf ~/.yarn
   echo "(대체로)삭제 완료"
 else
